62ff1c60e665f374839f5301208defc83f32c3f4
Shan <Shan@ShansPC>
2025-01-02 14:57:55 -0500

    Java basic scripts

---
+++ JavaProject STD/src/JavaProgramSTD/Program2.java
@@ -0,0 +1,34 @@
+package JavaProgramSTD;
+
+import JavaPackage.Study;
+
+public class Program2 {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a = 100;
+			int b = 23;
+			int ab = 100;
+			int bb = 300;
+			int c = a-b;
+			System.out.println ("Subtraction " + c);
+			
+			
+			if (ab>bb)
+			System.out.println ("The number is Smaller " +a);
+			
+			else
+				
+			System.out.println ("The number is bigger " +b);
+			
+			System.out.println ("im getting better at this dawggg") ;
+			
+			System.out.println ("testing") ;
+			
+		}
+
+	}
+
+
+
